74c3fccb50abf3ca34e051d75dc90943c5b4dafe
quantinx <josephtassell@gmail.com>
2024-08-07 15:18:13 +0200

    fix: added underline compatibility with reader

--- client/src/components/RichTextViewer/RichTextViewer.jsx
+++ client/src/components/RichTextViewer/RichTextViewer.jsx
@@ -1,10 +1,11 @@
 import { EditorContent, useEditor } from "@tiptap/react";
+import Underline from "@tiptap/extension-underline";
 import StarterKit from "@tiptap/starter-kit";
 
 export default function RichTextViewer({ content }) {
   const parsedContent = JSON.parse(content);
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [StarterKit, Underline],
     content: parsedContent,
     editable: false,
   });
