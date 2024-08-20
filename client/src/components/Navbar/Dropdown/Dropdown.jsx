172582684d061a40c59e1742effffe39f7bbec19
quantinx <josephtassell@gmail.com>
2024-08-20 23:15:36 +0200

    style: redesigned dropdown to be more responsive

--- client/src/components/Navbar/Dropdown/Dropdown.jsx
+++ client/src/components/Navbar/Dropdown/Dropdown.jsx
@@ -9,26 +9,28 @@ export default function Dropdown({
 }) {
   return (
     <>
-      <div className="dropdown-container">
-        {visible && !loading && !error && (
-          <ul className="dropdown-links">
-            {categories.map((category, i) => {
-              return (
-                <li key={i} className="dropdown-link-container">
-                  <Link
-                    onClick={hideDropdown}
-                    className="dropdown-link"
-                    to={"/categories/" + category.stack_name}
-                  >
-                    {category.stack_name}
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        )}
-        {loading && visible && <div>Please wait</div>}
-        {error && visible && <div>Something went wrong :c</div>}
+      <div className="dropdown-parent">
+        <div className="dropdown-container">
+          {visible && !loading && !error && (
+            <ul className="dropdown-links">
+              {categories.map((category, i) => {
+                return (
+                  <li key={i} className="dropdown-link-container">
+                    <Link
+                      onClick={hideDropdown}
+                      className="dropdown-link"
+                      to={"/categories/" + category.stack_name}
+                    >
+                      {category.stack_name}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          )}
+          {loading && visible && <div>Please wait</div>}
+          {error && visible && <div>Something went wrong :c</div>}
+        </div>
       </div>
     </>
   );
