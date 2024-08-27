e2f418f446a94ac11e56723596f342d7d3c48625
quantinx <josephtassell@gmail.com>
2024-08-27 16:45:01 +0200

    fix: added missing mutator  to profile page

--- client/src/pages/Profile/EditProfileModal/EditProfileModal.jsx
+++ client/src/pages/Profile/EditProfileModal/EditProfileModal.jsx
@@ -1,21 +1,43 @@
 import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+import sendData from "../../../helpers/sendData";
 import "./EditProfileModal.css";
 
 export default function EditProfileModal({ user, visible, closeModal }) {
   const [nameField, setNameField] = useState(user.username);
+  const [fullNameField, setFullNameField] = useState(user.full_name);
   const [profilePictureUrl, setProfilePictureUrl] = useState(
     user.profile_picture
   );
   const [profileContent, setProfileContent] = useState(user.profile_content);
+  const [buttonEnabled, setButtonEnabled] = useState(true);
+
+  const [message, setMessage] = useState("");
+
+  const profileMutator = useMutation({
+    mutationKey: "profile",
+    mutationFn: ({ url, method, payload }) => sendData(url, method, payload),
+    onSuccess: (response) => {
+      setMessage("Profile updated successfully!");
+      setButtonEnabled(true);
+    },
+  });
 
   function handleSave(e) {
     e.preventDefault();
     const payload = {
       username: nameField,
       profile_picture: profilePictureUrl,
+      full_name: fullNameField,
       profile_content: profileContent,
     };
     console.log(payload);
+    setButtonEnabled(false);
+    profileMutator.mutate({
+      url: "/api/users/",
+      method: "PATCH",
+      payload: payload,
+    });
   }
   return (
     <>
@@ -37,6 +59,20 @@ export default function EditProfileModal({ user, visible, closeModal }) {
                 id="username"
                 className="edit-profile-input"
                 value={nameField}
+                onChange={(e) => {
+                  setNameField(e.target.value);
+                }}
+              />
+            </div>
+            <div className="edit-profile-input-container">
+              <label className="edit-profile-label" htmlFor="fullname">
+                Full name
+              </label>
+              <input
+                name="fullname"
+                id="fullname"
+                className="edit-profile-input"
+                value={fullNameField}
                 onChange={(e) => {
                   setNameField(e.target.value);
                 }}
@@ -73,7 +109,12 @@ export default function EditProfileModal({ user, visible, closeModal }) {
                 }}
               />
             </div>
-            <button className="edit-profile-button" onClick={handleSave}>
+            <div>{message}</div>
+            <button
+              className="edit-profile-button"
+              onClick={handleSave}
+              disabled={!buttonEnabled}
+            >
               Save
             </button>
           </form>
