50e65445ceb13d80946163ace0696520ddee65f5
quantinx <josephtassell@gmail.com>
2024-08-29 14:13:51 +0200

    feat: added backend support for user profiles

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,7 +2,11 @@ const express = require("express");
 const Joi = require("joi");
 const userRouter = express.Router();
 
-const { getUserDataForProfile, updateUser } = require("../db/userHelpers");
+const {
+  getUserDataForProfile,
+  updateUser,
+  getProfileByUsername,
+} = require("../db/userHelpers");
 userRouter.get("/", async (req, res) => {
   if (!req.isAuthenticated) {
     res.status(200).json({ loggedIn: false });
@@ -12,6 +16,12 @@ userRouter.get("/", async (req, res) => {
   const user = req.user.user_ID;
   const userData = await getUserDataForProfile(user);
   res.status(userData.status).json({ loggedIn: true, data: userData.data });
+});
+
+userRouter.get("/profile/:username", async (req, res) => {
+  const username = req.params.username;
+  const userProfle = await getProfileByUsername(username);
+  res.status(userProfle.status).json(userProfle.data);
 });
 
 userRouter.patch("/", async (req, res) => {
