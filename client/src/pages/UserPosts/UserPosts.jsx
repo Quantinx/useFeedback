e973a31723a7a5e632a691377b7f7225dd47aea9
quantinx <josephtassell@gmail.com>
2024-08-29 16:56:50 +0200

    feat:added basic pagination to userposts

--- client/src/pages/UserPosts/UserPosts.jsx
+++ client/src/pages/UserPosts/UserPosts.jsx
@@ -2,30 +2,56 @@ import { useParams } from "react-router-dom";
 import getData from "../../helpers/getData";
 import PostPreview from "../../components/Post/Post";
 import "./UserPosts.css";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useInfiniteQuery } from "@tanstack/react-query";
 export default function UserPosts() {
   const { username } = useParams();
   const queryURL = "/api/posts?username=" + username;
-  const { data, isLoading, isError } = useQuery({
-    queryKey: [queryURL],
-    queryFn: () => getData(queryURL),
-    refetchOnWindowFocus: false,
-    refetchOnMount: true,
-    staleTime: 120000,
-  });
+  const { data, isLoading, isError, fetchNextPage, hasNextPage } =
+    useInfiniteQuery({
+      queryKey: [queryURL],
+      queryFn: ({ pageParam }) => getData(queryURL + "&page=" + pageParam),
+      initialPageParam: 1,
+      refetchOnWindowFocus: false,
+      refetchOnMount: true,
+      staleTime: 120000,
+      getNextPageParam: (lastPage) => {
+        const { currentPage, perPage, to } = lastPage.pagination;
+        const hasNextPage = to >= currentPage * perPage;
+        return hasNextPage ? Number(currentPage) + 1 : undefined;
+      },
+      getPreviousPageParam: (firstPage) => {
+        const { currentPage } = firstPage.pagination;
+        return currentPage > 1 ? currentPage - 1 : undefined;
+      },
+    });
 
+  function handlePageChange() {
+    if (!hasNextPage) {
+      alert("No new page avail");
+    }
+    fetchNextPage();
+  }
+  console.log(data);
   return (
     <>
       <article className="user-post-page-container">
         <h2 className="user-post-page-heading">{username}</h2>
+
         {!isLoading && !isError && (
           <div>
-            {data.data.map((post, i) => {
-              return <PostPreview post={post} key={i} />;
+            {data.pages.map((page, i) => {
+              return (
+                <div key={i}>
+                  {page.data.map((post, j) => {
+                    return <PostPreview post={post} key={j} />;
+                  })}
+                </div>
+              );
             })}
           </div>
         )}
       </article>
+      <button onClick={handlePageChange}>Next page</button>
     </>
   );
 }
